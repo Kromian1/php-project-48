bbe55c356c02f664db23ece759f9ae863b16210c
Kromian1 <mok1408@yandex.ru>
2025-11-24 22:05:23 +0300

    completed 3-st step

--- src/Parser.php
+++ src/Parser.php
@@ -8,9 +8,9 @@ class Parser
     public function parse(string $pathFile): mixed
     {
         $content = file_get_contents($pathFile);
-        $extention = pathinfo($pathFile, PATHINFO_EXTENSION);
+        $extension = pathinfo($pathFile, PATHINFO_EXTENSION);
 
-        return match ($extention) {
+        return match ($extension) {
             'json' => json_decode($content, false),
             default => throw new \Exception("Unknown extension\n")
         };
