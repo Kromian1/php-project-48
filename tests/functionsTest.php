05b35cf888db5e89f79c08f94cfd596d2315c4a4
Михаил Кузнецов <m.kuznetsov@MacBook-Air-Mihail.local>
2025-12-04 15:24:22 +0300

    finished tests functionsTest.php & some fixes functions.php

--- tests/functionsTest.php
+++ tests/functionsTest.php
@@ -11,13 +11,25 @@ class functionsTest extends TestCase
 {
     private const FIXTURESDIR = __DIR__ . '/fixtures/';
     #[DataProvider('mainFlowProvider')]
-    public function testGenDiff(string $pathFile1, string $pathFile2, string $expectedDifferJsons): void
+    public function testMainFlow(string $pathFile1, string $pathFile2, string $expectedDifferJsons): void
     {
         $actual = genDiff($pathFile1, $pathFile2);
 
         $this->assertEquals($expectedDifferJsons, $actual);
     }
 
+    public function testGendiffWithNotExistingFile(): void
+    {
+        $tempFile = tempnam(sys_get_temp_dir(), 'tempFile');
+        $nonExistingFile = sys_get_temp_dir() . '/non_existing_' . uniqid() . '.json';
+
+        $this->expectException(\RuntimeException::class);
+        gendiff($nonExistingFile, $tempFile);
+
+        if (file_exists($tempFile)) {
+            unlink($tempFile);
+        }
+    }
     public static function mainFlowProvider(): array
     {
         $expectedDifferJsons = <<<EXPECTED
